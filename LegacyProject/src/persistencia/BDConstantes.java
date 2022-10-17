dc5128c74098dd2716047aa8aa17585d0563b6f4
Noelia Díaz Alejo Alejo <USER@172.19.155.170>
2022-10-17 13:56:16 +0200

    Creación de la interface BDConstantes -Convertimos la clase BDConstantes en una interface. -Añadimos algunos de los atributos de Agente (url y driver). -Modificamos algunas de sus constantes(sustituir admin por root, e iso1_db por iso). -Añadir comentarios.

--- LegacyProject/src/persistencia/BDConstantes.java
+++ LegacyProject/src/persistencia/BDConstantes.java
@@ -1,9 +1,13 @@
 package persistencia;
 
-public class BDConstantes {
-	final static String DRIVER ="jdbc:derby";
+public interface BDConstantes {
+	// Driven para conectar con bases de datos MySQL
+	final static String DRIVER ="com.mysql.cj.jdbc.Driver";
 	final static String CONNECTION_STRING ="jdbc:derby:iso1_db;create=true";
-	final static String DBNAME ="iso1_db";
-	final static String DBUSER ="admin";
-	final static String DBPASS ="admin";
+	final static String DBNAME ="iso";
+	final static String DBUSER ="root";
+	final static String DBPASS ="root"; // instancia del agente
+	// Identificador ODBC de la base de datos
+	final static String URL = "jdbc:mysql://localhost:3306/iso?user=root&password=root";
+
 }
